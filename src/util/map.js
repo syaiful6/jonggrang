47374ea0df3a62e5b3e086d42b45d510db974cff
Syaiful Bahri <sbahri@indo-soft.com>
2016-08-11 15:40:20 +0700

    fix map util

--- src/util/map.js
+++ src/util/map.js
@@ -8,7 +8,7 @@ var isArray = Array.isArray || function(a) {
 // look like ramda fail to map a stream, so build it here
 module.exports = curryN(2, function map(fun, functor) {
   if (functor.fmap) return functor.fmap(fun)
-  if (functor.map && !isArray(functor)) functor.map(fun)
+  if (functor.map && !isArray(functor)) return functor.map(fun)
   // otherwise pass to rambda
   return rmap(fun, functor)
 })
