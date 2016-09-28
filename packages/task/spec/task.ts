cd3fc428677b0224468bd200e78e0e97737fce73
Syaiful Bahri <syaiful@pengunjungblog.com>
2016-09-29 02:12:44 +0700

    move utility test

--- packages/task/spec/task.ts
+++ packages/task/spec/task.ts
@@ -1,59 +1,7 @@
-import { Task, Handler } from '../lib/core'
 import { expect } from 'chai'
 import { property } from 'jsverify'
-
-function failRes(x: any) {
-  throw new Error(`Invalidly entered resolution branch with value ${x}`);
-}
-
-function failRej(x: any) {
-  throw new Error(`Invalidly entered rejection branch with value ${x}`);
-}
-
-const noop = () => {}
-
-// because we run this test on node, process.nextTick is safe to simulate async task
-function onNextTick<T>(v: T) {
-  return new Task((rej: Handler<any>, resolve: Handler<T>) => {
-    process.nextTick(() => resolve(v))
-    return noop
-  })
-}
-
-function assertEqual(a: Task<any, any>, b: Task<any, any>) {
-  return new Promise(done => {
-    const task = a.and(b)
-    task.fork(failRej, v => {
-      expect(v[0]).to.equal(v[1])
-      done(true)
-    })
-  })
-}
-
-function lift<T, R>(f: (v: T) => R) {
-  return function (a: T) {
-    return Task.of(f(a))
-  }
-}
-
-function rejectOf<T>(a: T) {
-  return new Task((reject: Handler<any>, resolve: Handler<T>) => {
-    reject(a)
-    return noop
-  })
-}
-
-function computeCallStack(): number {
-  try {
-    return 1 + computeCallStack()
-  } catch (_) {
-    return 1
-  }
-}
-const MAX_STACK = computeCallStack()
-function identity<T>(t: T) {
-  return t
-}
+import { Task, Handler } from '../lib/core'
+import { assertEqual, identity, lift } from './helpers/util'
 
 describe('Task', () => {
   let taskC: Task<any, number>
@@ -77,7 +25,9 @@ describe('Task', () => {
       expect(typeof taskC.map).to.be.equal('function')
     })
     property(
-      'compose', 'nat', 'nat -> nat', 'nat -> nat',
+      'compose',
+      'nat',
+      'nat -> nat', 'nat -> nat',
       (v: number, a: (v: number) => number, u: (v: number) => number) => {
         let vt = Task.of(v)
         let ut = Task.of(u)
