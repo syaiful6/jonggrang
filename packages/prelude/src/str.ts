d1fb1de18d1b9ee50d71b6b8481a85943d233101
Syaiful Bahri <syaiful@pengunjungblog.com>
2018-01-20 10:25:41 +0700

    add takeWhile to Prelude str

--- packages/prelude/src/str.ts
+++ packages/prelude/src/str.ts
@@ -1,4 +1,11 @@
-import { Maybe, nothing, just } from './maybe';
+import { Maybe, nothing, just, isJust } from './maybe';
+
+/**
+ * Check if the given string is empty
+ */
+export function isEmpty(s: string): boolean {
+  return s.length === 0;
+}
 
 /**
  * Returns the index of the first occurrence of the pattern in the
@@ -6,6 +13,15 @@ import { Maybe, nothing, just } from './maybe';
  */
 export function indexOf(pattern: string, subj: string): Maybe<number> {
   const ix = subj.indexOf(pattern);
+  return ix === -1 ? nothing : just(ix);
+}
+
+/**
+ * Returns the index of the last occurrence of the pattern in the
+ * given string. Returns `Nothing` if there is no match.
+ */
+export function lastIndexOf(pattern: string, subj: string): Maybe<number> {
+  const ix = subj.lastIndexOf(pattern);
   return ix === -1 ? nothing : just(ix);
 }
 
@@ -19,10 +35,46 @@ export function drop(n: number, s: string): string {
 }
 
 /**
- * eturns the first `n` characters of the string.
+ * Returns the first `n` characters of the string.
  * @param n number
  * @param s number
  */
 export function take(n: number, s: string): string {
   return s.substr(0, n);
+}
+
+/**
+ * Returns the number of contiguous characters at the beginning
+ * of the string for which the predicate holds.
+ */
+export function count(pred: (_: string) => boolean, s: string): number {
+  let i = 0;
+  while (i < s.length && pred(s.charAt(i))) {
+    i++;
+  }
+  return i;
+}
+
+/**
+ * Returns the longest prefix (possibly empty) of characters that satisfy
+ * the predicate.
+ */
+export function takeWhile(pred: (_: string) => boolean, s: string): string {
+  return take(count(pred, s), s);
+}
+
+/**
+ * Returns the suffix remaining after `takeWhile`.
+ */
+export function dropWhile(pred: (_: string) => boolean, s: string): string {
+  return drop(count(pred, s), s);
+}
+
+/**
+ * If the string starts with the given prefix, return the portion of the
+ * string left after removing it, as a Just value. Otherwise, return Nothing.
+ */
+export function stripPrefix(pattern: string, str: string): Maybe<string> {
+  const ix = indexOf(pattern, str);
+  return isJust(ix) && ix.value === 0 ? just(drop(pattern.length, str)) : nothing;
 }
--- packages/prelude/test/browser/str.ts
+++ packages/prelude/test/browser/str.ts
@@ -5,6 +5,16 @@ import * as S from '../../src/str';
 import { just, nothing } from '../../src/maybe';
 
 describe('Prelude Str', () => {
+  describe('isEmpty', () => {
+    it('Return true if passed empty string', () => {
+      expect(S.isEmpty('')).to.be.true;
+    });
+
+    it('Return false is passed non empty string', () => {
+      expect(S.isEmpty('ab')).to.be.false;
+    });
+  });
+
   describe('drop', () => {
     it('return same string if pass 0 or less', () => {
       expect(S.drop(0, 'ab')).to.be.equals('ab');
@@ -50,5 +60,32 @@ describe('Prelude Str', () => {
     it('Return nothing if the pattern didn\'t occur in given string', () => {
       expect(S.indexOf('cb', 'abcd')).to.be.deep.equals(nothing);
     })
+  });
+
+  describe('count', () => {
+    it('work with empty string and return zero', () => {
+      expect(S.count(x => x === 'a', '')).to.be.equals(0);
+      expect(S.count(() => true, '')).to.be.equals(0);
+    });
+
+    it('return the number of contiguous characters at the beginning that predicate hold', () => {
+      expect(S.count(a => a === 'a', 'ab')).to.be.equals(1);
+      expect(S.count(a => a === 'a', 'aaab')).to.be.equals(3);
+      expect(S.count(a => a === 'a', 'aabaaa')).to.be.equals(2);
+    });
+  });
+
+  describe('takeWhile', () => {
+    it('return whole string if predicate always return true', () => {
+      expect(S.takeWhile(() => true, 'abc')).to.be.equals('abc');
+    });
+
+    it('return empty string if predicate always return false', () => {
+      expect(S.takeWhile(() => false, 'abc')).to.be.equals('');
+    });
+
+    it('returns the longest prefix that satisfy predicate', () => {
+      expect(S.takeWhile(a => a !== 'b', 'aabbaa')).to.be.equals('aa');
+    });
   });
 });
